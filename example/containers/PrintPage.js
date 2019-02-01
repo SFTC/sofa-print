46d12bdc5b5a91fa60fd739aa6449f7daa3bde80
Leona Yang <leonayang@sf-express.com>
2019-02-01 11:44:42 +0800

    add printPage

--- example/containers/PrintPage.js
+++ example/containers/PrintPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styled, { css } from 'styled-components';
+import Print from '../../src/printPage';
 
 const PageWrapper = styled.div`
   padding: 20px;
@@ -15,43 +16,49 @@ class PrintPage extends Component {
   render() {
     const { orderList, skuList } = this.state;
     return (
-      <PageWrapper id="print-wrapper">
-        {orderList.map((order) => (
-          <div key={order}>
-            <h2>order detail table</h2>
-            <table>
-              <thead>
-                <tr>
-                  <th>column 1</th>
-                  <th>column 2</th>
-                  <th>column 3</th>
-                  <th>column 4</th>
-                  <th>column 5</th>
-                  <th>column 6</th>
-                  <th>column 7</th>
-                  <th>column 8</th>
-                  <th>column 9</th>
-                </tr>
-              </thead>
-              <tbody>
-                {skuList.map((sku) => (
-                  <tr key={sku}>
-                    <td>data 1</td>
-                    <td>data 2</td>
-                    <td>data 3</td>
-                    <td>data 4</td>
-                    <td>data 5</td>
-                    <td>data 6</td>
-                    <td>data 7</td>
-                    <td>data 8</td>
-                    <td>data 9</td>
+      <div>
+        <Print
+          trigger={() => <a href="#">Print this out!</a>}
+          content={() => this.componentRef}
+        />
+        <PageWrapper id="print-wrapper" ref={el => (this.componentRef = el)}>
+          {orderList.map((order) => (
+            <div key={order}>
+              <h2>order detail table</h2>
+              <table>
+                <thead>
+                  <tr>
+                    <th>column 1</th>
+                    <th>column 2</th>
+                    <th>column 3</th>
+                    <th>column 4</th>
+                    <th>column 5</th>
+                    <th>column 6</th>
+                    <th>column 7</th>
+                    <th>column 8</th>
+                    <th>column 9</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        ))}
-      </PageWrapper>
+                </thead>
+                <tbody>
+                  {skuList.map((sku) => (
+                    <tr key={sku}>
+                      <td>data 1</td>
+                      <td>data 2</td>
+                      <td>data 3</td>
+                      <td>data 4</td>
+                      <td>data 5</td>
+                      <td>data 6</td>
+                      <td>data 7</td>
+                      <td>data 8</td>
+                      <td>data 9</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          ))}
+        </PageWrapper>
+      </div>
     );
   }
 }
